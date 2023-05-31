e18b5bbc61a8d64e6421519fced51087cbdea918
ikundaE <enoch.ikunda@gmail.com>
2023-05-31 11:25:47 -0400

    loop.js file

---
+++ loop.js
@@ -0,0 +1,13 @@
+/* 
+    ? Loops
+    * allow us to execute a block of code repeatedly
+    * they stop once an exit condition has been met
+*/
+
+/* 
+    ?For loop
+*/
+
+for (let i = 0; i <= 10; i = i + 1){
+    console.log(i);
+}
