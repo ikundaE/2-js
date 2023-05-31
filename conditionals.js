bcd1e06c443d7185341b03a3c267bb68d296b13f
ikundaE <enoch.ikunda@gmail.com>
2023-05-31 11:25:23 -0400

    loop.js file

--- conditionals.js
+++ conditionals.js
@@ -159,3 +159,124 @@ season == 1
     ! you MUST have an else statement
 
 */
+
+/* 
+    ? Ternary Challenge
+
+    * Use the age from earlier to check if:
+    * You're under 18 you're still a teenager
+    * If you're over 18 you can travel but cannot drink
+    * If you're over 21 you can drink alcohol
+*/
+let age = 40;
+// let result;
+age >= 18 && age < 21 
+    ? console.log("\nyou can travel but cannot drink") 
+    : age >= 21 
+    ? console.log("\nyou can drink alcohol")
+    : console.log("\nyou're still a teenager");
+
+    function isLeapYear (year) {
+        (((year % 4) == 0 && (year % 100) != 0) || (year % 400) == 0)
+          ? console.log (year + " is a leap year.")
+          : console.log (year + " is not a leap year.")
+      }
+
+    isLeapYear (2022);
+    isLeapYear (3489);
+    isLeapYear (1998);
+    isLeapYear (1088);
+    isLeapYear (123);
+    isLeapYear (2000);
+    isLeapYear (1900);
+
+/* 
+    ? Switch Statements
+    * switches execute a block of code dependent upon a different case
+    * we ask question that requires a specific response determined by our answer
+*/
+
+let uprightInstructor = "Paul";
+
+switch (uprightInstructor) {
+	// information that cases are compared against
+	case "Paul":
+		// condition that's comapred against the switch
+		// executes the code below
+		console.log(`\n ${uprightInstructor} is a Lead SD Instructor`);
+		break;
+	// used to stop other case from evaluating
+	case "Rob":
+		console.log(`\n ${uprightInstructor} has dad jokes`);
+		break;
+	case "Henry":
+		console.log(`\n ${uprightInstructor} is a Teaching Assistant`);
+		break;
+	default:
+        // equivalent of an else statement; a catch-all
+		console.log(`\n ${uprightInstructor} may or may not work here.`);
+}
+
+/* 
+    ? Switch Challenge
+
+    * Create a grade variable that holds a value (ex: 78)
+
+    * Create a switch statement that checks against the variable.
+
+    * If it's above 89, console log "You got an A"
+    * greater than 79 B
+    * greater than 66 C
+    * greater than 59 D
+    * below 59 F
+    * default statement of "Input is not a grade"
+    
+    If you cannot create a switch, try an if/else or a ternary first
+*/
+
+let gradeWithValue = "potato";
+
+switch (true){
+    case (gradeWithValue > 89):
+        console.log(`\n ${gradeWithValue} is an A` );
+        break;
+    case (gradeWithValue > 79):
+        console.log(`\n ${gradeWithValue} is a B` );
+        break;
+    case (gradeWithValue > 69):
+        console.log(`\n ${gradeWithValue} is a C` );
+        break;
+    case (gradeWithValue > 59):
+        console.log(`\n ${gradeWithValue} is a D` );
+        break;
+    case (gradeWithValue < 59):
+        console.log(`\n ${gradeWithValue} is a F` );
+        break;
+    default:
+        console.log (`\n ${gradeWithValue} is not a grade!`)
+}
+
+
+// Hello Frenemy Solution
+function respond(name) {
+	// Going further sanitize input
+	name = name.toLowerCase();
+	// Going futher sanitize output
+	let result = name.split(" ");
+	console.log(`\n[${result}]`);
+	result = `${result[0][0].toUpperCase()}${result[0].slice(1)} ${result[1][0].toUpperCase()}${result[1].slice(1)}`;
+	if (name == "darth vader" || name == "lex luthor") {
+		console.log(`\nNoooo, ${result}, go away!`);
+	} else {
+		console.log(`\nHello, ${result}`);
+	}
+    /* 
+    name == "darth vader" || name == "lex luthor"
+		? console.log(`Noooo, ${result}, go away!`)
+		: console.log(`Hello, ${result}`);
+    */
+}
+
+respond("Darth Vader");
+respond("lex luthor");
+respond("lUKE sKYWALKER");
